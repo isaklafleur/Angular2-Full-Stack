5de1acab2f59783249648aa9ea7787929fc18613
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-09 09:48:12 +0100

    .

--- assets/app/messsages/message.component.ts
+++ assets/app/messsages/message.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from './message.model';
 
 @Component({
     selector: 'app-message',
@@ -19,5 +20,6 @@ import { Component } from '@angular/core';
     `]
 })
 export class MessageComponent {
+    message: Message
     
 }
