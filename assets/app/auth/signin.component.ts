aa0077cf78fbec26bd9703fc5227cc0acdb7ea66
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-09 22:28:59 +0100

    final commit.

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 export class SigninComponent implements OnInit {
-    constructor() { }
+    myForm: FormGroup;
 
-    ngOnInit() { }
+    onSubmit() {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, Validators.required),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -10,16 +10,14 @@ export class SignupComponent implements OnInit {
 
     onSubmit() {
         console.log(this.myForm);
+        this.myForm.reset();
     }
 
     ngOnInit() {
         this.myForm = new FormGroup({
             firstName: new FormControl(null, Validators.required),
             lastName: new FormControl(null, Validators.required),
-            email: new FormControl(null, [
-                Validators.required,
-                Validators.pattern('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/')
-            ]),
+            email: new FormControl(null, Validators.required),
             password: new FormControl(null, Validators.required)
         });
     }
